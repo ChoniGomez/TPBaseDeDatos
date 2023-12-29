61038eb801bbd54c0cf6cf2dfdd5add1c90bd5b3
ChoniGomez <chonidacharygomez@gmail.com>
2023-12-29 02:09:35 -0300

    Proyecto Agregado

---
+++ Blog Posts/post-2.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US"><head><!-- Created by Artisteer v4.3.0.60745 -->
+    <meta charset="utf-8">
+    <title>Conse ctetur</title>
+    <meta name="viewport" content="initial-scale = 1.0, maximum-scale = 1.0, user-scalable = no, width = device-width">
+
+    <!--[if lt IE 9]><script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <link rel="stylesheet" href="../style.css" media="screen">
+    <!--[if lte IE 7]><link rel="stylesheet" href="../style.ie7.css" media="screen" /><![endif]-->
+    <link rel="stylesheet" href="../style.responsive.css" media="all">
+
+
+    <script src="../jquery.js"></script>
+    <script src="../script.js"></script>
+    <script src="../script.responsive.js"></script>
+
+
+
+<style>.art-content .art-postcontent-0 .layout-item-0 { padding-right: 10px;padding-left: 10px;  }
+.ie7 .art-post .art-layout-cell {border:none !important; padding:0 !important; }
+.ie6 .art-post .art-layout-cell {border:none !important; padding:0 !important; }
+
+</style></head>
+<body>
+<div id="art-main">
+    <div id="art-hmenu-bg" class="art-bar art-nav">
+    </div>
+<header class="art-header">
+
+    <div class="art-shapes">
+        <div class="art-object920242836"></div>
+<div class="art-object1238046981"></div>
+<div class="art-object820124297"></div>
+<div class="art-object372277643"></div>
+
+            </div>
+
+<h1 class="art-headline">
+    <a href="/">Headline</a>
+</h1>
+<h2 class="art-slogan">Slogan</h2>
+
+
+
+
+
+                        
+                    
+</header>
+<div class="art-sheet clearfix">
+<nav class="art-nav">
+    <div class="art-nav-inner">
+    <ul class="art-hmenu"><li><a href="../home.php" class="">Home</a></li><li><a href="../alta-pelicula.php" class="">Alta Pelicula</a></li><li><a href="../modificar-pelicula.php" class="">Modificar Pelicula</a></li><li><a href="../listar-peliculas.php" class="">Listar Peliculas</a></li></ul> 
+        </div>
+    </nav>
+<div class="art-layout-wrapper">
+                <div class="art-content-layout">
+                    <div class="art-content-layout-row">
+                        <div class="art-layout-cell art-sidebar1"><div class="art-block clearfix">
+        <div class="art-blockheader">
+            <h3 class="t">Block</h3>
+        </div>
+        <div class="art-blockcontent"><p>Enter Block content here...</p>
+<br>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra, tellus sit amet congue vulputate, nisi erat iaculis nibh, vitae feugiat sapien ante eget mauris.</p></div>
+</div></div>
+                        <div class="art-layout-cell art-content"><article class="art-post art-article">
+                                <div class="art-postmetadataheader">
+                                        <h2 class="art-postheader"><span class="art-postheadericon">Conse ctetur</span></h2>
+                                    </div>
+                                                <div class="art-postheadericons art-metadata-icons">
+                    <span class="art-postdateicon"></span>
+                     | <span class="art-postauthoricon"> <a href="#" title="Posts by Admin">Admin</a></span>
+                     | <span class="art-postpdficon"></span>
+                     | <span class="art-postprinticon"></span>
+                     | <span class="art-postemailicon"></span>
+                     | <span class="art-postediticon"></span>
+                                    </div>
+                <div class="art-postcontent art-postcontent-0 clearfix"><div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 100%" >
+        <div class="image-caption-wrapper" style="width: 55%; float: left"><img alt="an image" style="width: 100%; max-width:360px" class="art-lightbox" src="../images/shutterstock_32050237.png"></div>
+        <p>Lorem <sup>superscript</sup> dolor <sub>subscript</sub>&nbsp;amet, consectetuer adipiscing elit, <a href="#" title="test link">test link</a>. Nullam dignissim convallis est. Quisque aliquam. <cite>cite</cite>. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl.</p>
+        <p>Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. <abbr title="National Basketball Association">NBA</abbr> Mauris a ante.<!--CUT--></p>
+        <p>Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus. <abbr title="Avenue">AVE</abbr><br></p>
+    </div>
+    </div>
+</div>
+</div>
+                                <div class="art-postmetadatafooter">
+                                        <div class="art-postfootericons art-metadata-icons">
+                        <span class="art-postcategoryicon">Category: No Category</span>
+                         | <span class="art-posttagicon">Tags: <a href="#" title="link">link</a>, <a href="#" title="visited link" class="visited">visited</a>, <a href="#" title="hovered link" class="hover">hovered</a></span>
+                         | <span class="art-postcommentsicon"><a href="#comments" title="Comments">1 Comments »</a></span>
+                                            </div>
+                                    </div>
+                
+
+</article></div>
+                    </div>
+                </div>
+            </div>
+    </div>
+<footer class="art-footer">
+  <div class="art-footer-inner">
+<p><a href="#">Link1</a> | <a href="#">Link2</a> | <a href="#">Link3</a></p>
+<p>Copyright © 2017. All Rights Reserved.</p>
+    <p class="art-page-footer">
+        <span id="art-footnote-links"><a href="http://www.artisteer.com/" target="_blank">Web Template</a> created with Artisteer.</span>
+    </p>
+  </div>
+</footer>
+
+</div>
+
+
+</body></html>
---
+++ alta-pelicula.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US"><head><!-- Created by Artisteer v4.3.0.60745 -->
+    <meta charset="utf-8">
+    <title>Página Nueva</title>
+    <meta name="viewport" content="initial-scale = 1.0, maximum-scale = 1.0, user-scalable = no, width = device-width">
+
+    <!--[if lt IE 9]><script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <link rel="stylesheet" href="style.css" media="screen">
+    <!--[if lte IE 7]><link rel="stylesheet" href="style.ie7.css" media="screen" /><![endif]-->
+    <link rel="stylesheet" href="style.responsive.css" media="all">
+
+
+    <script src="jquery.js"></script>
+    <script src="script.js"></script>
+    <script src="script.responsive.js"></script>
+
+
+
+</head>
+<body>
+<div id="art-main">
+    <div id="art-hmenu-bg" class="art-bar art-nav">
+    </div>
+<header class="art-header">
+
+    <div class="art-shapes">
+        <div class="art-object920242836"></div>
+<div class="art-object1238046981"></div>
+<div class="art-object820124297"></div>
+<div class="art-object372277643"></div>
+
+            </div>
+
+<h1 class="art-headline">
+    <a href="/">Headline</a>
+</h1>
+<h2 class="art-slogan">Slogan</h2>
+
+
+
+
+
+                        
+                    
+</header>
+<div class="art-sheet clearfix">
+<nav class="art-nav">
+    <div class="art-nav-inner">
+    <ul class="art-hmenu"><li><a href="home.php" class="">Home</a></li><li><a href="alta-pelicula.php" class="active">Alta Pelicula</a></li><li><a href="modificar-pelicula.php">Modificar Pelicula</a></li><li><a href="listar-peliculas.php">Listar Peliculas</a></li></ul> 
+        </div>
+    </nav>
+<div class="art-layout-wrapper">
+                <div class="art-content-layout">
+                    <div class="art-content-layout-row">
+                        <div class="art-layout-cell art-sidebar1"><div class="art-block clearfix">
+        <div class="art-blockheader">
+            <h3 class="t">Block</h3>
+        </div>
+        <div class="art-blockcontent"><p>Enter Block content here...</p>
+<br>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra, tellus sit amet congue vulputate, nisi erat iaculis nibh, vitae feugiat sapien ante eget mauris.</p></div>
+</div></div>
+                        <div class="art-layout-cell art-content"><article class="art-post art-article">
+                                <div class="art-postmetadataheader">
+                                        <h2 class="art-postheader"><span class="art-postheadericon">Página Nueva</span></h2>
+                                    </div>
+                                                
+                <div class="art-postcontent art-postcontent-0 clearfix"><p><br/></p></div>
+                                
+                
+
+</article></div>
+                    </div>
+                </div>
+            </div>
+    </div>
+<footer class="art-footer">
+  <div class="art-footer-inner">
+<p><a href="#">Link1</a> | <a href="#">Link2</a> | <a href="#">Link3</a></p>
+<p>Copyright © 2017. All Rights Reserved.</p>
+    <p class="art-page-footer">
+        <span id="art-footnote-links"><a href="http://www.artisteer.com/" target="_blank">Web Template</a> created with Artisteer.</span>
+    </p>
+  </div>
+</footer>
+
+</div>
+
+
+</body></html>
